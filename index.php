fe43e8bdc9f35fd60d3b192628deb1e8e50e100f
mohamedgb007 <gb007mohamedselma900@yahoo.fr>
2014-03-12 14:15:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns:="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>	
+	<title>home</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="mondersky" />
+	<link rel="stylesheet" href="css/design.css?<?php echo date('l jS \of F Y h:i:s A'); ?>" type="text/css"/>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/utilities.js"></script>
+	<script>
+			
+	</script>
+</head> 
+<body>
+mohamed el hadi
+</body>
+<script>
+
+</script>
+</html>
